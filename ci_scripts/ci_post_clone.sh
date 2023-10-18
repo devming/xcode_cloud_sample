1da657f827dd23db7ffddf13e6d1bf80eb9c02c5
devming <cmk330@gmail.com>
2023-10-18 18:14:30 +0900

    add ci_script files

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Coffee
+#
+#  Created by devming on 10/18/23.
+#  
---
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  ci_pre_xcodebuild.sh
+#  Coffee
+#
+#  Created by devming on 10/18/23.
+#  
