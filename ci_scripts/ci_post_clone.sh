7663be4808c41090dd724744ff6f63749d230059
devming <cmk330@gmail.com>
2023-10-18 21:42:25 +0900

    ci update

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,9 +6,9 @@
 #  Created by devming on 10/18/23.
 #  
 
-curl -sL https://firebase.tools | upgrade=true bash
+curl -sL https://firebase.tools | bash
 
-firebase login:ci
+firebase login
 
 firebase appdistribution:distribute $CI_DEVELOPMENT_SIGNED_APP_PATH/Coffee.ipa --app 1:150620856531:ios:7654e882fed44a1799b426 --release-notes "Test 배포"
 
